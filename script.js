d7b2b5eda9f7cbdd5f940b49e80811e8621c0bea
Andrzej Gebala <andrzej.gebala35@gmail.com>
2021-10-03 17:31:53 +0200

    kolejne zmiany

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const img1 = document.querySelector('.item1');
+const img2 = document.querySelector('.item2');
+const btnArrow = document.querySelector('.arrow');
+const srcArrow = document.querySelector('.fas');
+const sectionPhoto = document.querySelector('.photos');
+const text1 = document.createElement('h1');
+
+
+document.body.append(text1)
+text1.textContent = 'witam wsztskich...:)'
+text1.classList.add('hideText');
+
+
+const fun1 = () =>{
+    img1.classList.toggle('hide');
+    img1.classList.toggle('itemChange');
+    img2.classList.toggle('itemChange2');
+    srcArrow.classList.toggle('fa-arrow-up');
+    text1.classList.toggle('hideText');
+}
+
+
+btnArrow.addEventListener('click', fun1);
 
 
 
